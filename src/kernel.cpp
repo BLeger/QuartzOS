bcde219ca0f7d3e52e93fe92240dd9f70af3555f
Benoit LEGER <benoit.leger@hotmail.fr>
2019-09-13 17:49:34 -0400

    Better printf

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,14 +1,51 @@
 #include "types.h"
 #include "GlobalDescriptorTable.h"
 
+#define SCREEN_WIDTH 80
+#define SCREEN_HEIGHT 25
+
+void clear_screen() 
+{
+    static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+
+    for (int x = 0; x < SCREEN_WIDTH; x++) 
+    {
+        for (int y = 0; y < SCREEN_HEIGHT; y++) 
+        {
+            VideoMemory[SCREEN_WIDTH * y + x] = (VideoMemory[SCREEN_WIDTH * y + x] & 0xFF00) | ' ';
+        }
+    }
+}
+
 void printf(const char* str)
 {
-    uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+    static uint16_t* VideoMemory = (uint16_t*) 0xb8000;
+
+    // Position on the screen
+    static uint8_t cursor_x = 0, cursor_y = 0;
 
     for (int i = 0; str[i] != '\0'; i++) 
     {
-        // Keep the high bits (color) and write character
-        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+        if (str[i] == '\n') {
+            cursor_x = 0;
+            cursor_y++;
+        } else {
+            // Keep the high bits (color) and write character
+            VideoMemory[SCREEN_WIDTH * cursor_y + cursor_x] = (VideoMemory[SCREEN_WIDTH * cursor_y + cursor_x] & 0xFF00) | str[i];
+            cursor_x++;
+        }
+
+        if (cursor_x >= SCREEN_WIDTH) {
+            cursor_x = 0;
+            cursor_y++;
+        }
+        
+        if (cursor_y >= SCREEN_HEIGHT) 
+        {
+            cursor_x = 0;
+            cursor_y = 0;
+            clear_screen();
+        }
     }
 }
 
@@ -23,7 +60,9 @@ extern "C" void callConstructors()
 
 extern "C" void kmain(void* multibooot_structure, uint32_t magic_number) 
 {
-    printf("Welcome to Quartz OS");
+    clear_screen();
+
+    printf("Welcome to Quartz OS\n");
 
     GlobalDescriptorTable gdt();
 
